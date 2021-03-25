0d3c54713d6c847922868cafced644022aadfa21
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-25 00:44:50 -0300

    Finalizando component de texto e seus histories

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -5,4 +5,60 @@ export const Title = styled.Text`
   color: white;
   font-family: 'Montserrat-Bold';
 `;
+export const InputLabel = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(16)}px;
+  color: white;
+  font-family: 'Montserrat-SemiBold';
+`;
+
+export const ButtonText = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(22)}px;
+  color: white;
+  font-family: 'Montserrat-Bold';
+`;
+
+// Detail Texts
+
+export const DetailTitle = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(24)}px;
+  color: white;
+  font-family: 'Montserrat-Bold';
+`;
+
+export const DetailSubTitle = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(18)}px;
+  color: white;
+  font-family: 'Montserrat-SemiBold';
+`;
+
+export const DetailText = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(14)}px;
+  color: white;
+  font-family: 'Montserrat-Regular';
+`;
+
+export const DetailSectionTitle = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(20)}px;
+  color: white;
+  font-family: 'Montserrat-Bold';
+`;
+
+// CardTexts
+export const CardTitle = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(14)}px;
+  color: white;
+  font-family: 'Montserrat-Bold';
+`;
+
+export const CardDescription = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(10)}px;
+  color: white;
+  font-family: 'Montserrat-Regular';
+`;
+
+export const CardHighLightText = styled.Text`
+  font-size: ${({ theme }) => theme.metrics.px(16)}px;
+  color: white;
+  font-family: 'Montserrat-Bold';
+`;
 
