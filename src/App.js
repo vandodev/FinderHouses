901baf19e4054e4811b3a70429a8866aa08456b0
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-25 00:04:29 -0300

    Instalando e configurando Storybook

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {SplashScreen} from './screens/index';
 import {ThemeProvider} from 'styled-components';
 import { theme } from './styles/theme';
+import StoryBook from '../storybook';
 
 const App = () => {
     return (
       <ThemeProvider theme={theme}>
-        <SplashScreen />
+        <StoryBook />
       </ThemeProvider>
    
   );
