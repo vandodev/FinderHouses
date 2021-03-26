d9ea070c61c4e5db80e07eb7a0670df47617298b
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-26 02:21:40 -0300

    Criando comunicação com api

---
+++ src/services/calls.js
@@ -0,0 +1,17 @@
+import api from './api';
+export const getHousesCall = async () =>{
+    try {
+        const result = await api.get('properties/v2/list-for-rent', {
+            query:{
+                city: 'Miami',
+                state_code: 'FL',
+                limit: 15,
+                offset,
+                sort: 'relevance',
+            },
+        });
+        return result.data;
+    } catch (error) {
+        return error
+    }
+}
